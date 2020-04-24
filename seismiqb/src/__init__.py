9b216a6d3b2e1df7683f68362355c9b017553e60
Alexander Koryagin <akoriagin@nes.ru>
2020-04-24 13:54:40 +0300

    remove plot- utils-file; add missing import

--- seismiqb/src/__init__.py
+++ seismiqb/src/__init__.py
@@ -5,5 +5,6 @@ from .geometry import SeismicGeometry
 from .horizon import UnstructuredHorizon, StructuredHorizon, Horizon
 from .facies import GeoBody
 from .metrics import HorizonMetrics, GeometryMetrics
+from .plotters import plot_image
 from .utils import * # pylint: disable=wildcard-import
 from .plot_utils import * # pylint: disable=wildcard-import
