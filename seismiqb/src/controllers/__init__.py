9e361c4fa4f11587a677f972ecb4dec63dc952a3
Darima Mylzenova <darimamylzenova@gmail.com>
2020-07-13 12:56:22 +0000

    add extractor to init

--- seismiqb/src/controllers/__init__.py
+++ seismiqb/src/controllers/__init__.py
@@ -3,4 +3,5 @@ from .base import BaseController #pylint: disable=import-error
 from .interpolator import CarcassInterpolator, GridInterpolator
 from .enhancer import Enhancer
 from .extender import Extender
+from .extractor import Extractor
 from .best_practices import * #pylint: disable=wildcard-import
