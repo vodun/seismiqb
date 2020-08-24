f8451a45d5952d80717f34d66c2df5a9ab9b0e17
Sergey Tsimfer <Tsimfer.SA@gazprom-neft.ru>
2020-08-24 14:50:46 +0300

    Update horizon assessment script

--- scripts/horizon_assessment.py
+++ scripts/horizon_assessment.py
@@ -17,8 +17,8 @@ from seismiqb import METRIC_CMAP, enlarge_carcass_metric, plot_image
 
 
 # Help message
-MSG = """Compute a complete quality report on a set of horizons, and, if needed, set of carcasses.
-Each of the horizons is matched to exactly one carcass, if those provided.
+MSG = """Compute a complete quality report on a set of horizons, and, if needed, set of other horizons.
+Each of the horizons is matched to exactly one of others, if those provided.
 For each of the evaluated surfaces, a number of simple statistics like length and coverage are computed,
 as well as metric maps. For carcass-like surfaces, metrics are enlarged for visual clarity.
 The report is saved into passed directory and contains a CSV table with numeric results,
@@ -29,7 +29,7 @@ metric images, and, if needed, horizons and metric point clouds.
 ARGS = [
     ('cube-path', 'path to the seismic cube in HDF5 format', str, None),
     ('horizon-path', 'path to the horizon in a seismic cube in CHARISMA format', [str], None),
-    ('carcass-path', 'path to the horizon in a seismic cube in CHARISMA format', [str], []),
+    ('other-path', 'path to the horizon in a seismic cube in CHARISMA format', [str], []),
     ('savedir', 'path to save files to', str, '_placeholder_'),
     ('metrics', 'which metrics to compute', str, ['support_corrs']),
     ('add-prefix', 'whether to prepend horizon name to the saved file names', str2bool, True),
@@ -62,7 +62,7 @@ if __name__ == '__main__':
     safe_mkdir(config['savedir'])
 
     horizons = [Horizon(path, geometry=geometry) for path in config['horizon-path']]
-    carcasses = [Horizon(path, geometry=geometry) for path in config['carcass-path']]
+    others = [Horizon(path, geometry=geometry) for path in config['other-path']]
     dataframe = []
 
     for horizon in horizons:
@@ -97,41 +97,42 @@ if __name__ == '__main__':
         if config['save-files']:
             horizon.dump(os.path.join(config['savedir'], horizon.name), add_height=False)
 
-        # Try to detect the carcass of a horizon and evaluate it
-        if carcasses:
-            lst = [(carcass, Horizon.check_proximity(horizon, carcass))
-                   for carcass in carcasses]
+        # Try to detect the carcass/prediction of a horizon and evaluate it
+        if others:
+            lst = [(other, Horizon.check_proximity(horizon, other))
+                   for other in others]
             lst.sort(key=lambda x: abs(x[1].get('mean', 999999)))
-            carcass, overlap_info = lst[0]
+            other, overlap_info = lst[0]
             if overlap_info['mean'] < 10:
                 row_dict = {
                     **row_dict,
-                    'carcass_name': carcass.name,
-                    'carcass_path': os.path.join(*carcass.path.split('/')[-2:]),
+                    'other_name': other.name,
+                    'other_path': os.path.join(*other.path.split('/')[-2:]),
                     'average_l1': overlap_info['mean'],
                 }
-                horizon.show(savepath=os.path.join(config['savedir'], f'{prefix}carcass_depthmap.png'))
+                other_prefix = 'carcass' if other.is_carcass else ''
+                other.show(savepath=os.path.join(config['savedir'], f'{prefix}{other_prefix}_depthmap.png'))
 
-                cm = HorizonMetrics(carcass)
+                om = HorizonMetrics(other)
                 for metric_name in config['metrics']:
                     kwargs = copy(LOCAL_KWARGS) if metric_name.startswith('local') else copy(SUPPORT_KWARGS)
                     kwargs = {} if metric_name.startswith('insta') else kwargs
-                    savepath = os.path.join(config['savedir'], f'{prefix}carcass_{metric_name}')
+                    savepath = os.path.join(config['savedir'], f'{prefix}{other_prefix}_{metric_name}')
 
-                    metric = cm.evaluate(metric_name, **kwargs)
-                    metric = metric if len(carcass) > (len(horizon) // 10) else enlarge_carcass_metric(metric, geometry)
+                    metric = om.evaluate(metric_name, **kwargs)
+                    metric = enlarge_carcass_metric(metric, geometry) if other.is_carcass else metric
                     plot_image(metric, figsize=(20, 20),
                             cmap=METRIC_CMAP, zmin=-1, zmax=1, fill_color='black',
                             savepath=savepath + '.png')
 
-                    row_dict[f'carcass_{metric_name}'] = np.nanmean(metric)
+                    row_dict[f'{other_prefix}_{metric_name}'] = np.nanmean(metric)
                 if config['save-files']:
-                    horizon.dump(os.path.join(config['savedir'], f'{horizon.name}_carcass'), add_height=False)
+                    horizon.dump(os.path.join(config['savedir'], f'{horizon.name}_{other_prefix}'), add_height=False)
             else:
                 row_dict = {
                     **row_dict,
-                    'carcass_name': '',
-                    'carcass_path': '',
+                    'other_name': '',
+                    'other_path': '',
                     **{metric_name: 0 for metric_name in config['metrics']},
                 }
 
