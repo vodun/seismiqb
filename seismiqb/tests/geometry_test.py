20b3e696b657b6b62d4f996e3abb440dc4ee4270
HollowPrincess <risahawkeye@mail.ru>
2021-10-15 18:16:09 +0300

    fix message in github

--- seismiqb/tests/geometry_test.py
+++ seismiqb/tests/geometry_test.py
@@ -19,13 +19,14 @@ def test_geometry(capsys):
     """ Run SeismicGeometry test notebook."""
     # Delete old test notebook results
     previous_output_files = glob.glob(os.path.join(TESTS_SCRIPTS_DIR, 'notebooks/geometry_test_out_*.ipynb'))
+
     for file in previous_output_files:
         os.remove(file)
 
     out_path_ipynb = os.path.join(TESTS_SCRIPTS_DIR, f'notebooks/geometry_test_out_{DATESTAMP}.ipynb')
 
     # Tests execution
-    exec_info = run_notebook(
+    _, tests_notebook = run_notebook(
         path=os.path.join(TESTS_SCRIPTS_DIR, 'notebooks/geometry_test.ipynb'),
         nb_kwargs={
             'TEST_FOLDER': TEST_FOLDER,
@@ -36,34 +37,27 @@ def test_geometry(capsys):
         },
         insert_pos=0,
         out_path_ipynb=out_path_ipynb,
-        display_links=False
+        display_links=False,
+        return_nb=True
     )
 
+    # Get message and timing from the notebook cells (github didn't save temporary files)
+    msg = tests_notebook['cells'][7]['outputs'][0]['text']
+    timings = json.loads(tests_notebook['cells'][9]['outputs'][0]['text'].replace('\'', '\"'))
+
     with capsys.disabled():
-        # Extract and drop message
         if SHOW_MESSAGE:
-            message_path = glob.glob(os.path.join(TEST_FOLDER, 'message*.txt'))[-1]
-            with open(message_path, "r") as infile:
-                for line in infile.readlines():
-                    print(line)
+            print(msg)
 
-        if DROP_EXTRA_FILES:
-            os.remove(message_path)
-
-        # Extract timings data
-        timings_path = glob.glob(os.path.join(TEST_FOLDER, 'timings_*.json'))
-        timings_path = sorted(timings_path)[-1]
-        with open(timings_path, "r") as infile:
-            timings = json.load(infile)
-            pp = pprint.PrettyPrinter()
-            pp.pprint(timings)
+        pp = pprint.PrettyPrinter()
+        pp.pprint(timings)
 
         # Output message and extra file deleting
         if timings['state']=='OK':
-            print('Tests were executed successfully.\n')
+            print('Tests for SeismicGeometry were executed successfully.\n')
 
             if DROP_EXTRA_FILES:
                 os.remove(out_path_ipynb)
         else:
-            print(f'An ERROR occured in cell number {exec_info}:\n{out_path_ipynb}\n')
+            print(f'SeismicGeometry tests failed:\n{out_path_ipynb}\n')
             assert False
