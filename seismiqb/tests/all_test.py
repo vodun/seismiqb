bc59a129b80b3f2241b45103cd72bbc6d8fd2a5c
HollowPrincess <risahawkeye@mail.ru>
2022-03-03 11:52:31 +0300

    simplify

--- seismiqb/tests/all_test.py
+++ seismiqb/tests/all_test.py
@@ -35,16 +35,13 @@ SHOW_TEST_ERROR_INFO : bool
 SAVE_LOGS_REG_EXP : list of str
     A list of regular expressions for files which should be saved after a test execution.
 
-Visualizations in saved execution notebooks are controlled with:
+Outputs in saved execution notebooks are controlled with:
 
 SCALE : int
     Figures scale.
 SHOW_FIGURES : bool
     Whether to show additional figures.
     Showing some figures can be useful for finding out the reason for the failure of tests.
-
-Text outputs in executed notebooks controlled with:
-
 VERBOSE : bool
     Whether to print information about successful tests during the execution of the cycles.
 """
@@ -85,38 +82,30 @@ tests_params = {
 # Initialize tests configs
 geometry_formats = ['sgy', 'hdf5', 'qhdf5', 'blosc', 'qblosc']
 notebooks_params = (
-    # (notebook file, test params)
+    # (notebook filename, test params)
     # Note: params for each notebook in the test will be saved for it and for next notebooks in the test
 
     # CharismaMixin test
-    ('charisma_test.ipynb', {}),
+    ('charisma_test', {}),
 
     # SeismicGeometry test
-    ('geometry_test_preparation.ipynb', {'TEST_OUTPUTS': ['states', 'timings'], 'FORMATS': geometry_formats}),
-    *[('geometry_test_data_format.ipynb', {'FORMAT': f}) for f in geometry_formats],
-    ('geometry_test_final.ipynb', {}),
+    ('geometry_test_preparation', {'FORMATS': geometry_formats}),
+    *[('geometry_test_data_format', {'TEST_OUTPUTS': ['timings'], 'FORMAT': f}) for f in geometry_formats],
 
     # Horizon test
-    ('horizon_test_preparation.ipynb', {'TEST_OUTPUTS': ['message']}),
-    ('horizon_test_base.ipynb', {}),
-    ('horizon_test_attributes.ipynb', {}),
-    ('horizon_test_manipulations.ipynb', {}),
-    ('horizon_test_extraction.ipynb', {})
+    ('horizon_test_preparation', {}),
+    ('horizon_test_base', {}),
+    ('horizon_test_attributes', {}),
+    ('horizon_test_manipulations', {}),
+    ('horizon_test_extraction', {'TEST_OUTPUTS': ['message']})
 )
-
-
-# Global variables (for sharing between notebooks in one test)
-global_tests_states = {}
 
 
 @pytest.mark.parametrize("notebook_kwargs", notebooks_params)
 def test_run_notebook(notebook_kwargs, tmpdir_factory, capsys):
     """..!!.."""
-    # Variables and params initialization
-    notebook_basename, params_ = notebook_kwargs
-    test_name = notebook_basename.split('_')[0]
-    global_test_state = global_tests_states.get(test_name, {})
-    config = params_.copy()
+    filename, params = notebook_kwargs
+    config = params.copy()
     _ = config.pop('TEST_OUTPUTS', None)
 
     # Create `root_dir` for all `test_run_notebook` calls
@@ -127,15 +116,14 @@ def test_run_notebook(notebook_kwargs, tmpdir_factory, capsys):
             tests_params['TESTS_ROOT_DIR'] = os.path.join(tests_params['TESTS_SCRIPTS_DIR'], "tests_root_dir")
             os.makedirs(tests_params['TESTS_ROOT_DIR'], exist_ok=True)
 
-    # `params` initialization
-    params = global_test_state.get('globals', tests_params)
-    params.update(params_)
+    params.update(tests_params)
 
     # Run test notebook
-    path_ipynb = os.path.join(params['NOTEBOOKS_DIR'], notebook_basename)
-    file_name = os.path.splitext(notebook_basename)[0]
-    suffix = "_" + "_".join(str(k) + "_" + re.sub(r' ', '_', re.sub(r'[^\w^ ]', '', str(v))) for k,v in config.items())
-    out_path_ipynb = os.path.join(tests_params['TESTS_ROOT_DIR'], f"{file_name}_out{suffix}_{params['DATESTAMP']}.ipynb")
+    remove_symbols = lambda v: re.sub(r' ', '_', re.sub(r'[^\w^ ]', '', str(v)))
+    suffix = "_".join(f"{k}_{remove_symbols(v)}" for k,v in config.items())
+
+    path_ipynb = os.path.join(params['NOTEBOOKS_DIR'], f"{filename}.ipynb")
+    out_path_ipynb = os.path.join(params['TESTS_ROOT_DIR'], f"{filename}_out_{suffix}_{params['DATESTAMP']}.ipynb")
 
     exec_res = run_notebook(path=path_ipynb, inputs=params, outputs=params.get('TEST_OUTPUTS', []),
                             inputs_pos=2, out_path_ipynb=out_path_ipynb, display_links=False)
@@ -143,40 +131,23 @@ def test_run_notebook(notebook_kwargs, tmpdir_factory, capsys):
     if not exec_res['failed'] and params['REMOVE_EXTRA_FILES']:
         os.remove(out_path_ipynb)
 
-    # Update global test state
-    failed = global_test_state.get('failed', False) & exec_res['failed']
-    globals = global_test_state.get('globals', {})
-    globals.update(params)
-    globals.update(exec_res.get('outputs', {})) # For saving shared logs
-
-    state = {
-        'globals': globals,
-        'failed': failed,
-    }
-    if global_test_state:
-        global_tests_states[test_name].update(state)
-    else:
-        global_tests_states[test_name] = state
-
     # Terminal output
     with capsys.disabled():
-        print('\n' + params['DATESTAMP'])
-
         # Extract traceback if failed
-        if exec_res['failed'] and tests_params['SHOW_TEST_ERROR_INFO']:
+        if exec_res['failed'] and params['SHOW_TEST_ERROR_INFO']:
             print(exec_res.get('traceback', ''))
 
-        # Notebook outputs
+        # Test outputs
         if params['SHOW_MESSAGE']:
             for k, v in exec_res['outputs'].items():
+                print(f"{k}:\n")
                 if isinstance(v, str):
-                    print(f"{k}:\n{v}\n")
+                    print(v)
                 else:
-                    print(f"{k}:\n")
                     print(json.dumps(v, indent=4), '\n')
 
         # End of the running message
-        notebook_info = f"\'{notebook_basename}\'{' with config=' + str(config) if config else ''} was"
+        notebook_info = f"{params['DATESTAMP']} \'{filename}\'{' with config=' + str(config) if config else ''} was"
         if not exec_res['failed']:
             print(f"{notebook_info} executed successfully.\n")
         else:
