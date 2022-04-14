9204e1d1e3d3d8a266db19fee3183bb0e44cf74e
Sergey Tsimfer <Tsimfer.SA@gazprom-neft.ru>
2022-04-14 16:20:30 +0300

    Update transforms

--- seismiqb/crop_batch.py
+++ seismiqb/crop_batch.py
@@ -1181,31 +1181,30 @@ class SeismicCropBatch(Batch):
         return lfilter(b, a, crop, axis=1)
 
     @apply_parallel
-    def sign(self, crop):
+    def sign_transform(self, crop):
         """ Element-wise indication of the sign of a number. """
         return np.sign(crop)
 
     @apply_parallel
-    def analytic_transform(self, crop, axis=1, mode='phase'):
-        """ Compute instantaneous phase or frequency via the Hilbert transform.
-
-        Parameters
-        ----------
-        axis : int
-            Axis of transformation. Intended to be used after `rotate_axes`, so default value
-            is to make transform along depth dimension.
-        mode : str
-            If 'phase', compute instantaneous phase.
-            If 'freq', compute instantaneous frequency.
-        """
+    def instant_amplitudes_transform(self, crop, axis=-1):
+        """ Compute instantaneous amplitudes along the depth axis. """
         analytic = hilbert(crop, axis=axis)
-        phase = np.unwrap(np.angle(analytic))
+        return np.abs(analytic).astype(np.float32)
 
-        if mode == 'phase':
-            return phase
-        if 'freq' in mode:
-            return np.diff(phase, axis=axis, prepend=0) / (2*np.pi)
-        raise ValueError('Unknown `mode` parameter.')
+
+    @apply_parallel
+    def instant_phases_transform(self, crop, axis=-1):
+        """ Compute instantaneous phases along the depth axis. """
+        analytic = hilbert(crop, axis=axis)
+        return np.angle(analytic).astype(np.float32)
+
+    @apply_parallel
+    def frequencies_transform(self, crop, axis=-1):
+        """ Compute frequencies along the depth axis. """
+        analytic = hilbert(crop, axis=axis)
+        iphases = np.angle(analytic).astype(np.float32)
+        return np.diff(iphases, axis=-1, prepend=0) / (2 * np.pi)
+
 
     @apply_parallel
     def gaussian_filter(self, crop, axis=1, sigma=2, order=0):
@@ -1317,7 +1316,7 @@ class SeismicCropBatch(Batch):
         data = components.apply(lambda item: getattr(self, item)[idx].squeeze())
 
         if zoom_slice is not None:
-            data = [item[zoom_slice] for item in data]
+            data = data.apply(lambda item: item[zoom_slice])
 
         # Extract location
         location = self.locations[idx]
