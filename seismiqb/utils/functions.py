06d2b2113a3c42abcf011ef5ffe7278ad800f31f
AlexeyKozhevin <alexey.a.kozhevin@gmail.com>
2022-08-09 13:39:51 +0300

    Update seismiqb/utils/functions.py
    Co-authored-by: Sorokina Svetlana <risahawkeye@mail.ru>

--- seismiqb/utils/functions.py
+++ seismiqb/utils/functions.py
@@ -549,9 +549,9 @@ def insert_points_into_mask(mask, points, mask_bbox, width, axis):
         if ((point[0] >= mask_bbox[0][0] - left_margin[0]) and
             (point[1] >= mask_bbox[1][0] - left_margin[1]) and
             (point[2] >= mask_bbox[2][0] - left_margin[2]) and
-            (point[0] <  mask_bbox[0][1] + right_margin[0]-1) and
-            (point[1] <  mask_bbox[1][1] + right_margin[1]-1) and
-            (point[2] <  mask_bbox[2][1] + right_margin[2]-1)):
+            (point[0] <  mask_bbox[0][1] + right_margin[0] - 1) and
+            (point[1] <  mask_bbox[1][1] + right_margin[1] - 1) and
+            (point[2] <  mask_bbox[2][1] + right_margin[2] - 1)):
 
             point = point - mask_bbox[:, 0]
             left_bound = max(0, point[axis] - left_margin[axis])
